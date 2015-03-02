d188bb5eae003732364a5af09dc9eafb65e2f66b
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-03-02 22:39:57 +0100

    Adds mock module for pylibmc

--- conf.py
+++ conf.py
@@ -16,6 +16,20 @@
 import sys
 import os
 
+# mock modules with c extensions
+from mock import Mock as MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+
+MOCK_MODULES = ['pylibmc']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
