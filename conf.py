2304ae8d72e29f3d043d9ea36908d078953c147b
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-03-04 14:57:04 +0100

    Keep the source order in autodoc

--- conf.py
+++ conf.py
@@ -49,6 +49,8 @@ extensions = [
     'alabaster'
 ]
 
+autodoc_member_order = 'bysource'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
